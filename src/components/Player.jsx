236719cf19e126fa064cac7d2a9e7054014fe9ae
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-01-03 20:18:32 -0300

    feat: use useRef to handle user input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,27 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  //this playerName useRef will always be an object that has only the current property
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState();
+
+  function handleClick() {
+    //with playerName.current we can access all the properties that are exposed
+    //by the input html input the ref is used with
+    setEnteredPlayerName(playerName.current.value); //so this will get the value property from the input
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        {/*in ternary expressions like enteredPlayerName ? enteredPlayerName : "unknown entity"
+        we can use the following shotcut in js: */}
+        Welcome {enteredPlayerName ?? "unknown entity"}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
