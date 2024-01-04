76723bd51647002d10b6ff7deb91af3a2397b43f
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-01-04 12:02:34 -0300

    fix: ensure that each timer works individually

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     //targetTime is in seconds, setTimeout requires the time in ms
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     //this code bellow will execute right after the timer was set
     //only the function inside setTimeout wont execute now, only when the timer finishes
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+    //this is a js function that stops a timer, it just needs a pointer for that timer
+    //this pointer is returned by setTimeout
+    clearTimeout(timer.current);
   }
 
   return (
@@ -22,7 +30,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
